4efbe433ae4311b4b8e024e3a57242e0906d6a5d
bilizen1 <bilizen1@hotmail.com>
2017-02-05 12:24:47 -0500

    service animals

--- app.js
+++ app.js
@@ -3,12 +3,16 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = 3000;
 
 // App
 const app = express();
-app.get('/', function (req, res) {
+app.get('/users', function (req, res) {
   res.send('Este es un servicio GET muy bien ya sabes backend!! xD \n');
+});
+
+app.get('/animals', function (req, res) {
+  res.send('Este es un servicio animals!! xD \n');
 });
 
 app.listen(PORT);
