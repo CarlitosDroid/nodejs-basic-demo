51acbc3eeb835c117ffcb7d06ca6ac67d4780979
bilizen1 <bilizen1@hotmail.com>
2017-02-05 12:46:24 -0500

    deleted line

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 
 // Constants
-const PORT = 3000;
+const PORT = 80;
 
 // App
 const app = express();
@@ -11,13 +11,13 @@ app.get('/users', function (req, res) {
   res.send('Este es un servicio GET muy bien ya sabes backend!! xD \n');
 });
 
-<<<<<<< HEAD
+
 app.get('/windows', function (req, res) {
   res.send('windows \n');
-=======
+});
+
 app.get('/animals', function (req, res) {
   res.send('Este es un servicio animals!! xD \n');
->>>>>>> 4efbe433ae4311b4b8e024e3a57242e0906d6a5d
 });
 
 app.listen(PORT);
