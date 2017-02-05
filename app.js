8de95aeb299af150552f5661eaecab2fd54a0bc1
DavidTenorioRivas <david.tenorio.rivas@gmail.com>
2017-02-05 12:33:05 -0500

    Cambios

--- app.js
+++ app.js
@@ -11,5 +11,9 @@ app.get('/', function (req, res) {
   res.send('Este es un servicio GET muy bien ya sabes backend!! xD \n');
 });
 
+app.get('/windows', function (req, res) {
+  res.send('windows \n');
+});
+
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
