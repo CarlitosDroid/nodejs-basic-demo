90b102d7962a3730e971787a16e9e8754c6e678d
bilizen1 <bilizen1@hotmail.com>
2017-02-05 12:58:52 -0500

    status Carloncho

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 'use strict'
-
-const express = require('express');
-
-// Constants
-const PORT = 80;
-
-// App
-const app = express();
-app.get('/users', function (req, res) {
-  res.send('Este es un servicio GET muy bien ya sabes backend!! xD \n');
-});
-
-
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+ 
+ const express = require('express');
+ 
+ // Constants
+ const PORT = 8080;
+ 
+ // App
+ const app = express();
+ app.get('/', function (req, res) {
+   res.send('Este es un servicio GET muy bien ya sabes backend!! xD \n');
+ });
+ 
+ app.listen(PORT);
+ console.log('Running on http://localhost:' + PORT);
